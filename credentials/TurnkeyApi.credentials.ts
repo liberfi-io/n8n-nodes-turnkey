96fb798eedd812df9b01825f099bfe4aa0183622
quantum <quantum-01@outlook.com>
2025-08-30 20:29:25 +0800

    fix

--- credentials/TurnkeyApi.credentials.ts
+++ credentials/TurnkeyApi.credentials.ts
@@ -13,7 +13,7 @@ export class TurnkeyApi implements ICredentialType {
 			name: 'apiPublicKey',
 			type: 'string',
 			default: '',
-			typeOptions: { password: false },
+			typeOptions: { password: true },
 			description:
 				'Api Public Key of the Turnkey API',
 		},
